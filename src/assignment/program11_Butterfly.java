d41a3786f0b741f48e3a3f10054392d50a9cc2d3
shrutisehgal07 <shruti.sehgal@coditas.com>
2026-02-02 20:20:16 +0530

    program11_Butterfly.java

--- src/assignment/program11_Butterfly.java
+++ src/assignment/program11_Butterfly.java
@@ -24,46 +24,51 @@ public class program11_Butterfly {
 	System.out.println("Enter the length");
 	
 	int n= sc.nextInt();
-	
-	int space= 2*n-2;
-	int len= 2*n;
-	int star=0;
-	
-	for(int i=0;i<len;i++) {
-		if(i<n) {
-			space = space-2;
-			star++;
-		}
-		
 
-		else {
-			space = space + 2;
-			star--;
-		}
-		
-		for(int j=0;j<star;j++) {
-			System.out.print("*");
-		}
-		
-		for(int k=0;k<space;k++) {
-			System.out.print(" ");
-		}
-		
-		for(int j=0;j<star;j++) {
-			System.out.print("*");
-		}
-		if(i<n-1) {
-			space = space-2;
-		
-		}
-		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			for(int j=0;j<n-i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=n-i-1;j>0;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
 
-		else {
-			space = space + 2;
+			System.out.println();
 		}
-		
-		System.out.println();
-	}
+		for(int i=n-1;i>=0;i--)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			for(int j=0;j<n-i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=n-i-1;j>0;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+
+			System.out.println();
+
+		}
+
 	}
 	
 }
